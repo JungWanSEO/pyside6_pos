8f4e1d79e0019eeb380ecdd91b7b8cb636bf29c1
JungWanSEO <sjwan0326@gmail.com>
2024-11-22 00:36:28 +0900

    setting 및 연습
    python 설정 및 qml 이미지 적용하는 법

---
+++ example/imageRotate/QML_Main/main.py
@@ -0,0 +1,14 @@
+import sys
+from PySide6.QtGui import QGuiApplication
+from PySide6.QtQml import QQmlApplicationEngine
+
+if __name__ == "__main__":
+    app = QGuiApplication(sys.argv)
+    engine = QQmlApplicationEngine()
+    
+    engine.addImportPath(sys.path[0])
+    engine.loadFromModule("QML_Main", "Main")
+    
+    ex = app.exec()
+    del engine
+    sys.exit(ex)
