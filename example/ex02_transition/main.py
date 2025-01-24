2573fa35fbd9d0e322a58c792876954b80a6e865
JungWanSEO <36655724+JungWanSEO@users.noreply.github.com>
2025-01-24 16:49:42 +0900

    transition을 통한 animation 처리
    분류를 나눠서 코드 진행 가능

---
+++ example/ex02_transition/main.py
@@ -0,0 +1,13 @@
+import sys
+from PySide6.QtQml import QQmlApplicationEngine
+from PySide6.QtGui import QGuiApplication
+
+if __name__ == "__main__":
+    app = QGuiApplication(sys.argv)
+    engine = QQmlApplicationEngine()
+
+    engine.addImportPath(sys.path[0])
+    engine.loadFromModule("QML_Main", "Main")
+    ex = app.exec()
+    del engine
+    sys.exit(ex)
